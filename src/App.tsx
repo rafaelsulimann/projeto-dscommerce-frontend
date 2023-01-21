6bc2e05f3836239403ca73f6196216f5f50e95c0
Rafael Sulimann <sulimannrafael@gmail.com>
2023-01-20 21:20:20 -0300

    Routes ProductDetails

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import "./App.css";
-import computerImg from "./assets/produto.svg";
-import ButtonInverse from "./components/ButtonInverse";
-import ButtonPrimary from "./components/ButtonPrimary";
-import HeaderClient from "./components/HeaderClient";
-import ProductDetailsCard from "./components/ProductDetailsCard";
+import ProductDetails from "./routes/ProductDetails";
 
 export default function App() {
   return (
-    <>
-      <HeaderClient />
-      <main>
-        <section id="product-infos" className="generic-section">
-          <div className="container">
-            <ProductDetailsCard />
-          </div>
-        </section>
-        <section id="buttons" className="button-section">
-          <div className="container">
-            <ButtonPrimary />
-            <ButtonInverse />
-          </div>
-        </section>
-      </main>
-    </>
+    <ProductDetails />
   );
 }
